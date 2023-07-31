271b7d1b9abfe2d24b4e57d8077af470d6cfca35
Desmond Tong <tysdesmond2@gmail.com>
2023-07-31 23:06:14 +0800

    Create Watchlist component

--- src/Components/Watchlist.jsx
+++ src/Components/Watchlist.jsx
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Watchlist = () => {
   return (
     <>
-      <div className="row">watchlist</div>
+      <div className="row">
+        <div className="col-sm-9">
+            <p>Watchlist</p>
+        </div>
+        <div className="col-sm-3">
+            <button>Edit</button>
+        </div>
+      </div>
     </>
   );
 };
